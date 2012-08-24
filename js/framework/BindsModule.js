68bca6b8f607dd39b85cc0a0cdb964e6298c2f58
omar <omarisel01@gmail.com>
2012-08-23 23:10:45 -0500

    agregando toque final de action form

--- js/framework/BindsModule.js
+++ js/framework/BindsModule.js
@@ -102,9 +102,6 @@ bindsme.validate  = function(properties){
 
  submit : function(){
 
-console.log("here")
-
-   
 
 
   $(config.onSend).click(function(){
@@ -114,9 +111,12 @@ console.log("here")
 
     NUMBER_ERRORS =   $(config.form[0].object ).find(".error_object").length ;
     
-    if(validate.conf[0].VALID_DATA &&  NUMBER_ERRORS === 0  ){
+    if(validate.conf[0].VALID_DATA &&  NUMBER_ERRORS === 0  ){ // SI TODO ESA BIEN ENTTONCES ENVIAMOS LOS DATOS !!!!!!!!!!!!!!!!!!!!!!!!!!
 
-      console.log("todo bien!!")
+    
+
+        $(config.form[0].object ).attr("action", config.form[0].action );
+        $(config.form[0].object ).submit();
 
 
     }
@@ -322,7 +322,7 @@ console.log("here")
 
  	validateMail : function(obj){ // ESTA HACE QUE TENGA UN EMAIL CORRECTO
 
- console.log("validate");
+ 
         
     re=/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/
     
