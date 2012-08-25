190dbff5da146b1dd423ab412795f699c2fa3fa8
Omar <omarisel01@gmail.com>
2012-08-25 14:12:41 -0500

    agregando los archivos de binds

---
+++ js/contact/add_contact.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function($) {
+	
+
+	$(".add").click(function(){
+
+
+
+
+	 htmlcontent = '<input type="text" class="struct_search  " placeholder="Agrega contacto">';
+
+
+              binds.box({
+                                    title: "options.title ",
+                                    height:"73" , 
+                                    top : "39% ", 
+                                    content : htmlcontent ,
+
+                                     onClose : function(){
+
+
+
+
+                                     }
+                  }) ;  
+
+	});
+
+
+
+
+
+
+
+});
